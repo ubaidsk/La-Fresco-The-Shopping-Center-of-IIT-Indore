f2e1eace3fb4b68a4b854025c45b976606d02a3c
Shaikh-Ubaid <shaikhubaid769@gmail.com>
2019-10-22 00:29:59 +0530

    Fixed inconsistencies with small screens

--- setup.py
+++ setup.py
@@ -15,47 +15,44 @@ print("Opened Spreadsheet")
 
 @app.route("/")
 def Home():
-    worksheet_list =sh.worksheet("All Sheets").get_all_records()
+    worksheet_list = sh.worksheets()
+    all_sheet_list=worksheet_list[0].get_all_records()
     print("Got all worksheets")
     dictionary_of_items={}
-    for sheet_name in worksheet_list:
-        sheet=sh.worksheet(sheet_name.get("name"))
+    for row in all_sheet_list:
+        sheet=worksheet_list[row.get("id")]
         array_of_items=[]
         all_categories = sheet.get_all_records()
-        # print(all_categories)
         for category in all_categories:
             array_of_items.append(category)
-        dictionary_of_items[sheet_name.get("name")]=array_of_items
+        dictionary_of_items[row.get("name")]=array_of_items
     print("\n\n Dictionary of Items passed is\n")
     # print(dictionary_of_items)
     print("\n\n heading_names passed is\n")
     # print(worksheet_list)
-    return render_template('home.html', dictionary_of_items=dictionary_of_items, heading_names=worksheet_list)
+    return render_template('home.html', dictionary_of_items=dictionary_of_items, heading_names=all_sheet_list)
 
 @app.route('/items/<int:category_id>/<int:item_category_id>')
 def ShowItems(category_id, item_category_id):
-    worksheet_list =sh.worksheet("All Sheets").get_all_records()
+    worksheet_list = sh.worksheets()
+    all_sheet_list=worksheet_list[0].get_all_records()
     print("Got all worksheets")
     dictionary_of_items={}
-    for sheet_name in worksheet_list:
-        sheet=sh.worksheet(sheet_name.get("name"))
+    for row in all_sheet_list:
+        sheet=worksheet_list[row.get("id")]
         array_of_items=[]
         all_categories = sheet.get_all_records()
-        # print(all_categories)
         for category in all_categories:
             array_of_items.append(category)
-        dictionary_of_items[sheet_name.get("name")]=array_of_items
-    required_worksheet=sh.get_worksheet(category_id+1)
-    # print("required worksheet is",required_worksheet)
+        dictionary_of_items[row.get("name")]=array_of_items
+    required_worksheet=worksheet_list[category_id+1]
     needed_items=[]
     print("entering into for loop")
     for row in required_worksheet.get_all_records():
-        # print(row)
         if row.get("category_id")==item_category_id:
             needed_items.append(row)
-    # print("needed_items are ", needed_items)
-    for row in sh.get_worksheet(category_id).get_all_records():
+    for row in worksheet_list[category_id].get_all_records():
         if row.get("id")==item_category_id:
             item_title=row.get("name")
             break
-    return render_template('MenuItem.html', item_title=item_title, needed_items=needed_items, dictionary_of_items=dictionary_of_items, heading_names=worksheet_list)
+    return render_template('MenuItem.html', item_title=item_title, needed_items=needed_items, dictionary_of_items=dictionary_of_items, heading_names=all_sheet_list)
